ec6f7968295c776ed4d32a13308af1772a5fb9be
behr-eliot <behreliot@gmail.com>
2024-08-03 01:00:53 -0400

    Added nvim-surround

--- init.lua
+++ init.lua
@@ -877,7 +877,18 @@ require('lazy').setup({
     },
   },
   {
+<<<<<<< HEAD
     'jbyuki/nabla.nvim',
+=======
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+>>>>>>> 693744b (Added nvim-surround)
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
