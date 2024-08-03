634936a7d562bb4d01298c52c68c4af6d28a60b5
behr-eliot <behreliot@gmail.com>
2024-08-03 02:27:17 -0400

    vimtex *changed to skim pdf reader, disable quickfix

--- init.lua
+++ init.lua
@@ -878,7 +878,8 @@ require('lazy').setup({
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_quickfix_enabled = 0
     end,
   },
 
