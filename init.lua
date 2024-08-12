9775350f873f37248979f49ee165ca43280cbc8e
behr-eliot <behreliot@gmail.com>
2024-08-12 16:43:18 -0400

    Attempting wezterm image for quarto package

--- init.lua
+++ init.lua
@@ -883,12 +883,17 @@ require('lazy').setup({
     end,
   },
   {
+    'willothy/wezterm.nvim',
+    config = true,
+  },
+  {
     'benlubas/molten-nvim',
     version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
     build = ':UpdateRemotePlugins',
     init = function()
       -- this is an example, not a default. Please see the readme for more configuration options
       vim.g.molten_output_win_max_height = 12
+      vim.g.molten_image_provider = 'wezterm'
       vim.keymap.set('n', '<localleader>mi', ':MoltenInit<CR>', { silent = true, desc = 'Initialize the plugin' })
       vim.keymap.set('n', '<localleader>e', ':MoltenEvaluateOperator<CR>', { silent = true, desc = 'Run operator selection' })
       vim.keymap.set('n', '<localleader>rl', ':MoltenEvaluateLine<CR>', { silent = true, desc = 'Evaluate line' })
